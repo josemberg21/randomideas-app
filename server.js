5335fa3e333949068a9a44ab7633de3ba9dc5351
josemberg21 <francesco.falasca91@gmail.com>
2024-04-18 19:45:05 +0200

    Final Commit

--- server.js
+++ server.js
@@ -1,51 +1,32 @@
+const path = require('path');
 const express = require('express');
-const port = 5000;
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
 
-const ideas = [
-    {
-        id: 1,
-        text: 'Positive Newsletter, a newsletter that only shares positive, uplifting news',
-        tag: 'Technology',
-        username: 'josemberg21',
-        date: '2024-04-16'
-    },
-    {
-        id: 2,
-        text: 'Milk cartons that turn a different color the older that your milk is getting',
-        tag: 'Inventions',
-        username: 'SteveRogers',
-        date: '2022-01-02'
-    },
-    {
-        id: 3,
-        text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
-        tag: 'Software',
-        username: 'BruceBanner',
-        date: '2024-01-02'
-    }
-];
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// cors middleware
+app.use(cors({
+    origin: ['http://localhost:5000', 'http://localhost:3000'],
+    credentials: true
+}));
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the RandomIdeas API' });
 });
 
-// Get all ideas
-app.get('/api/ideas', (req, res) => {
-    res.json({ success: true, data: ideas });
-});
-
-// Get one idea
-app.get('/api/ideas/:id', (req, res) => {
-    const idea = ideas.find(idea => idea.id === +req.params.id);
-
-    if (!idea) {
-        return res
-            .status(404)
-            .json({ success: false, error: 'resource not found' });
-    }
-    res.json({ success: true, data: idea });
-});
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter)
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
